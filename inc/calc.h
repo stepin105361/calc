db01d28d64eb1febb1fb172d1a1dbe92d17df434
stepin105361 <69123968+stepin105361@users.noreply.github.com>
2020-11-10 22:43:45 +0530

    files added for the first time

---
+++ inc/calc.h
@@ -0,0 +1,39 @@
+#ifndef __CALCULATOR_OPERATION_H__
+
+#define __CALCULATOR_OPERATION_H__
+
+
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <stdio_ext.h>
+
+
+
+
+
+int add(int operand1, int operand2);
+
+
+
+
+
+int subtract(int operand1, int operand2);
+
+
+
+int multiply(int operand1, int operand2);
+
+
+
+
+
+
+
+int divide(int operand1, int operand2);
+
+
+
+#endif  /* #define __CALCULATOR_OPERATIONS_H__ */
